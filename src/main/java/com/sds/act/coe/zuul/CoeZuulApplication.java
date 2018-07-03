12ad8987521d6a951f57a1569f8518f6639c4d33
Giza Act <actmember@Gizas-iMac.local>
2018-07-03 13:37:04 +0900

    add corsFilter

--- src/main/java/com/sds/act/coe/zuul/CoeZuulApplication.java
+++ src/main/java/com/sds/act/coe/zuul/CoeZuulApplication.java
@@ -11,6 +11,14 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
@@ -36,5 +44,17 @@ public class CoeZuulApplication {
     @Bean
     public CoeRouterLocator coeRouterLocator() {
         return new CoeRouterLocator(serverProperties.getServlet().getServletPrefix(), zuulProperties);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    final CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.setAllowedOrigins(Collections.singletonList("*"));
+    config.setAllowedHeaders(Collections.singletonList("*"));
+    config.setAllowedMethods(Arrays.stream(HttpMethod.values()).map(HttpMethod::name).collect(Collectors.toList()));
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
     }
 }
